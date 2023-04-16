509f7dcc2f240814dc7079df28bb324b728af554
Rudis <82242977+krakatua@users.noreply.github.com>
2023-04-16 19:27:34 -0400

    fixing data

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -10,43 +10,66 @@ const ItemDetails = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  const {nftId} = useParams();
-  const [posts, setPost] = useState({});
+  const { nftId } = useParams();
+  const [posts, setPost] = useState([]);
+  console.log(nftId)
 
- console.log(nftId)
-  
+
 
   async function fetchPost() {
-    const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
-    setPost(...data)
+    const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
+    setPost(data)
     //data.filter(x => x.nftId === nftId).map(x=> console.log(x))
   }
   useEffect(() => {
     fetchPost()
   }, [])
 
+    
+
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].nftId === nftId) {
+      console.log(posts[i])
+    }
+  }
+
+ 
+
   
 
-  console.log(posts)
+
+
+ 
+
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
         <section arial-label="section"
-        className="mt90 sm-mt-0">
+          className="mt90 sm-mt-0">
           <div className="container">
             {
-              posts.Search.map((post) => (
-                <div>{post.nftImage}</div>
-              ))
+              Object.values(posts).map((post, index) => {
+                return (
+                  <div className="row" key={index}>
+                    <div className="col-md-6 text-center">
+                      <img
+                        src={post.authorImage}
+                        className="img-fluid img-rounded mb-sm-30 nft-image"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                )
+              })
             }
           </div>
 
         </section>
       </div>
     </div>
-    
+
   );
 };
 
